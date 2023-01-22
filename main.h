0328d8af7b27318750230f67b3fabda7c9bf0227
Temples-Dev <templelomotey@gmail.com>
2023-01-22 09:49:05 -0800

    prototypes added

--- main.h
+++ main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int checkchar(va_list charr);
+int checkstring(va_list string);
+int checkpercent(va_list percent);
 /*To Do -Project prototypes*/
 
 
