2085e0170f2d112fc85f431df959d31d0f489528
lowryel <eugeneagbaglo@gmail.com>
2023-01-22 06:37:15 -0800

    created an empty funtion

---
+++ print.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - return number of characters printed
+ * @format: character string
+ *
+ * Return: return number of characters rpinted excluding
+ * null bytes
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
