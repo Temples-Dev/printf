76b5f5b161e7f2b5e38d780fbe9f327721c31d0e
Temples-Dev <templelomotey@gmail.com>
2023-01-24 15:48:35 -0800

    task added

--- print.c
+++ print.c
@@ -12,19 +12,88 @@
 
 int _printf(const char *format, ...)
 {
-	char *str;
+int bytes_printed = 0;
+va_list args;
 
-	for (int index = 0; index < n; index++)
-	{
-		str = char*;
-		if (str == NULL)
-			printf("(nil)");
-		else
-			printf("%s", str);
+va_start(args, format);
 
-		if (int index != (n - 1) && format != NULL)
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
 
-			printf("%s", format);
-	}
-	printf("\n");
+for (const char *p = format; *p; p++)
+{
+if (*p != '%')
+{
+bytes_printed += _putchar(*p);
+continue;
+}
+p++;
+
+if (!*p)
+break;
+
+int width = 0, precision = -1;
+char flags = 0;
+int length_modifier = 0;
+
+if (*p == '0')
+{
+flags |= FLAG_ZERO;
+p++;
+}
+
+while (*p >= '0' && *p <= '9')
+{
+width = width * 10 + (*p - '0');
+p++;
+}
+
+if (*p == '.')
+{
+p++;
+precision = 0;
+while (*p >= '0' && *p <= '9')
+{
+precision = precision * 10 + (*p - '0');
+p++;
+}
+}
+
+if (*p == 'h' || *p == 'l' || *p == 'L')
+length_modifier = *p++;
+
+switch (*p)
+{
+case 'd':
+case 'i': {
+int value = va_arg(args, int);
+bytes_printed += print_int(value,
+flags, width, precision, length_modifier);
+break;
+}
+case 'c': {
+int value = va_arg(args, int);
+bytes_printed += print_char(value);
+break;
+}
+case 's': {
+char *value = va_arg(args, char *);
+bytes_printed += print_string(value,
+flags, width, precision);
+break;
+}
+case '%': {
+bytes_printed += _putchar('%');
+break;
+}
+default: {
+bytes_printed += _putchar(*p);
+break;
+}
+}
+}
+
+_putchar(BUF_FLUSH);
+va_end(args);
+return (bytes_printed);
 }
